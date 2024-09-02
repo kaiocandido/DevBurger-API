6a2a3186fd4ef4d5839cd70b89c8c8f718d50999
kaio candido <kaio.candf@gmail.com>
2024-09-02 11:55:23 -0300

    status pedido, validando usuario admin, e colocando validação na categorias de criação para usuario, atualizado os status do pedido e update do produto

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -2,6 +2,7 @@ import * as Yup from 'yup'
 import Order from '../schemas/Order'
 import Product from '../models/Products'
 import Category from '../models/Category'
+import User from '../models/User'
 
 class OrderController {
   async store(request, response) {
@@ -66,6 +67,42 @@ class OrderController {
     const createdOrder = await Order.create(order)
 
     return response.status(201).json(createdOrder)
+  }
+
+  async index(request, response) {
+    const orders = await Order.find()
+
+    return response.json(orders)
+  }
+
+  async update(request, response) {
+    const schema = Yup.object({
+      status: Yup.string().required(),
+    })
+
+    try {
+      await schema.validate(request.body, { abortEarly: false }) // Use validate para async
+    } catch (error) {
+      return response.status(400).json({ error: error.errors })
+    }
+
+    const { admin: isAdmin } = await User.findByPk(request.userId)
+
+    if (!isAdmin) {
+      return response.status(401).json()
+    }
+
+    const { id } = request.params
+
+    const { status } = request.body
+
+    try {
+      await Order.updateOne({ _id: id }, { status })
+    } catch (error) {
+      return response.status(400).json({ error: error.message })
+    }
+
+    return response.json({ message: 'Status update sucessufully' })
   }
 }
 
--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,12 +1,16 @@
 import * as Yup from 'yup'
 import Products from '../models/Products'
 import Category from '../models/Category'
+import User from '../models/User'
+
+//create
 class ProductController {
   async store(request, response) {
     const schema = Yup.object({
       name: Yup.string().required(),
       price: Yup.number().required(),
       category_id: Yup.number().required(),
+      offer: Yup.boolean(),
     })
 
     try {
@@ -15,17 +19,80 @@ class ProductController {
       return response.status(400).json({ error: error.errors })
     }
 
+    const { admin: isAdmin } = await User.findByPk(request.userId)
+
+    if (!isAdmin) {
+      return response.status(401).json()
+    }
+
     const { filename: path } = request.file
-    const { name, price, category_id } = request.body
+    const { name, price, category_id, offer } = request.body
 
     const product = await Products.create({
       name,
       category_id,
       price,
       path,
+      offer,
     })
 
     return response.status(201).json({ product })
+  }
+
+  //Update
+
+  async update(request, response) {
+    const schema = Yup.object({
+      name: Yup.string(),
+      price: Yup.number(),
+      category_id: Yup.number(),
+      offer: Yup.boolean(),
+    })
+
+    try {
+      schema.validateSync(request.body, { abortEarly: false })
+    } catch (error) {
+      return response.status(400).json({ error: error.errors })
+    }
+
+    const { admin: isAdmin } = await User.findByPk(request.userId)
+
+    if (!isAdmin) {
+      return response.status(401).json()
+    }
+
+    const { id } = request.params
+    const findProduct = await Products.findByPk(id)
+
+    if (!findProduct) {
+      return response
+        .staus(400)
+        .json({ error: 'make sure your product ID is correct' })
+    }
+
+    let path
+    if (request.file) {
+      path = request.file.filename
+    }
+
+    const { name, price, category_id, offer } = request.body
+
+    await Products.update(
+      {
+        name,
+        category_id,
+        price,
+        path,
+        offer,
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    )
+
+    return response.status(200).json()
   }
 
   async index(request, response) {
